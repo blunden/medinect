996491911b4e71ca5d472f82ed0b436bfcbd4672
blunden <blunden2@gmail.com>
2010-12-10 18:16:41 +0100

    Rename project and remove need for ugly symlink include

--- obj/Release/src/ofxKinect/libs/libfreenect/tilt.d
+++ obj/Release/src/ofxKinect/libs/libfreenect/tilt.d
@@ -3,13 +3,13 @@ obj/Release/src/ofxKinect/libs/libfreenect/tilt.d \
  src/ofxKinect/libs/libfreenect/tilt.c \
  src/ofxKinect/libs/libfreenect/freenect_internal.h \
  src/ofxKinect/libs/libfreenect/libfreenect.h \
- ../../../addons/ofxKinect/src/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h \
+ src//ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h \
  src/ofxKinect/libs/libfreenect/usb_libusb10.h
 
 src/ofxKinect/libs/libfreenect/freenect_internal.h:
 
 src/ofxKinect/libs/libfreenect/libfreenect.h:
 
-../../../addons/ofxKinect/src/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h:
+src//ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h:
 
 src/ofxKinect/libs/libfreenect/usb_libusb10.h:
---
+++ src/medinect.h
@@ -0,0 +1,41 @@
+#ifndef _MEDINECT
+#define _MEDINECT
+
+#include "ofMain.h"
+
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+
+class medinect : public ofBaseApp
+{
+	public:
+		void setup();
+		void draw();
+		void update();
+		void exit();
+		
+		void windowResized(int w, int h);
+		void keyPressed(int key);
+		
+		ofxKinect kinect;
+		
+		ofxCvContourFinder	contourFinder;
+		
+		ofxCvGrayscaleImage	grayImage;
+		ofxCvGrayscaleImage	grayThresh;
+		ofxCvGrayscaleImage	grayThreshFar;
+		
+		int	nearThreshold;
+		int	farThreshold;
+		
+		ofImage image1;
+		ofImage image2;
+		
+		ofTrueTypeFont verdana;
+	
+	private:
+	
+		int imageDisplayNumber;
+};
+
+#endif
